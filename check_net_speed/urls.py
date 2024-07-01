29fca664ccd13d2b577c64c17f7916df67ad081a
TheREX <shivamtherexpandey@gmail.com>
2024-07-01 19:47:37 +0530

    Changes:
    --> LightSpeed V2 Changed.

--- check_net_speed/urls.py
+++ check_net_speed/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('ping', views.Ping.as_view(), name='check_ping'),
     path('check-download', views.DownloadSpeed.as_view(), name='check_down'),
     path('check-upload', views.UploadSpeed.as_view(), name='check_upload'),
-    path('openspeedtest', views.OpenSpeedTest.as_view(), name='openspeedtest'),
 
     path('internet-speed-details', views.UserInternetSpeed.as_view(), name='user-details'),
 ]
